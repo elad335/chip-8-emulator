d55177b445eea25fc2b56685a70954467275f0f3
eladash <elad3356p@gmail.com>
2019-05-21 23:24:11 +0300

    MOVBE instruction usage

--- Chip-8 emulator/utils.h
+++ Chip-8 emulator/utils.h
@@ -265,10 +265,17 @@ inline u64 get_xgetbv(u32 xcr)
 #endif
 }
 
-// Checvk if CPU has AVX support (taken from https://github.com/RPCS3/rpcs3/blob/master/Utilities/sysinfo.cpp#L29)
+// Check if CPU has AVX support (taken from https://github.com/RPCS3/rpcs3/blob/master/Utilities/sysinfo.cpp#L29)
 static bool has_avx()
 {
 	static const bool g_value = get_cpuid(0, 0)[0] >= 0x1 && get_cpuid(1, 0)[2] & 0x10000000 && (get_cpuid(1, 0)[2] & 0x0C000000) == 0x0C000000 && (get_xgetbv(0) & 0x6) == 0x6;
+	return g_value;
+}
+
+// Check if CPU has MOVBE instruction support
+static bool has_movbe()
+{
+	static const bool g_value = (get_cpuid(1, 0)[2] & 0x400000) != 0;
 	return g_value;
 }
 
